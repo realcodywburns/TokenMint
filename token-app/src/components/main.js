7e2b7e4e3551e8d3d1efb95a668ba48feabbd5d9
Elaine Ou <elaine.ou@gmail.com>
2017-06-04 20:26:41 -0700

    src/components/tokens/launch: launch ico tab

--- token-app/src/components/main.js
+++ token-app/src/components/main.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Tabs, Tab, Grid, Row, Col } from 'react-bootstrap';
 import CreateToken from './tokens/create';
 import Information from './tokens/info';
+import LaunchIco from './tokens/launch';
 
 const Header = ({ logo }) => (
     <div className="App-header">
@@ -27,7 +28,9 @@ const Render = ({...props}) => (
                     <Tab eventKey={2} title="Create a Token">
                         <CreateToken />
                     </Tab>
-                    <Tab eventKey={3} title="Launch an ICO">Launch a crowdsale</Tab>
+                    <Tab eventKey={3} title="Launch an ICO">
+                        <LaunchIco />
+                    </Tab>
                     <Tab eventKey={4} title="Buy Tokens">ICO / Crowdsale Browser</Tab>
                     <Tab eventKey={5} title="Wallet Manager">Stuff that ClassicEtherWallet does</Tab>
                     <Tab eventKey={6} title="Exchange">Token Market</Tab>
