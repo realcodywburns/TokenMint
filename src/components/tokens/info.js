aabf0c35b115450d6831e92d70b910be02cef954
ChuckSRQ <ChuckSRQ@users.noreply.github.com>
2017-06-07 11:37:04 -0400

    Update info.js

--- src/components/tokens/info.js
+++ src/components/tokens/info.js
@@ -26,36 +26,29 @@ const Information = React.createClass({
           </Col>
           <Col sm={8}>
             <Tab.Content animation>
-              <Tab.Pane eventKey="prep">
+              <Tab.Pane eventKey="Preparation">
                 <ul>
-                  <li> Decide which account you wish all your free money to go to & ensure you have multiple backups of that account.</li>
-                  <li> Decide the maximum amount of ETC you are willing to make from your scam (your <u>Number of tokens</u>). Ensure that account has 0.01 ETC for gas.</li>
-                  <li> Start thinking of a name! The less vowels the better. Be sure to use j's, z's, and as many q's as you can . <strong> this is an important step </strong></li>
-                  <li> Do a roadshow! This means spam everyones slack, wechat, telegram and twitter relentlessly. Email people on lists you bought on the darknet or scraped from slack. Post an ANN on btctal. Really make everyone think that your product needs a token somehow instead of just using bitcoin or ether.</li>
+                  <li> Decide which Ethereum address you want your new tokens to go to & ensure you have multiple backups of that account. </li>
+                  <li> Decide the maximum amount of ETC you are willing to make from your scam (your Number of tokens). Ensure that account has 0.01 ETC or 0.01 ETH for gas, otherwise you will not be able to move your tokens. </li>
                 </ul>
               </Tab.Pane>
-              <Tab.Pane eventKey="launch">
+              <Tab.Pane eventKey="Launching a Token">
                 <ul>
-                  <li> Make your <strong>magic</strong>ERC20 token. The code is open to review so you wont be blamed for the 10's of thousands that get lost due to a bug that could be fixed with using the <a href="https://www.reddit.com/r/ethereum/comments/6c68mw/new_record_holder_appears_lets_congratulate/?st=j3edi4n2&sh=18d46ab0">simple fix in EIP223</a></li>
-                  <li> You will enter the <u>Token name</u>, <u>Actual number of tokens</u>, <u>Token symbol</u> for trading on markets, and the <u>decimal places</u>.</li>
-                  <li> That's it! load your wallet and move on the crowd sale! (Now is a good time to go test drive a new lambo, your doing great.)</li>
+                  <li> Make your ERC223 token. Any wallet or exchange that supports the ERC20 standard can support ERC223 tokens. ERC223 simply contains a fix to a bug that prevents tokens from being lost if they are sent to the token address. More information on ERC223 tokn standard can be <a href="https://www.reddit.com/r/ethereum/comments/60ql37/attention_be_careful_using_ethereum_tokens/">found here</a></li>
+                  <li> You will enter the Token name, Actual number of tokens, Token symbol for trading on markets, and the number of decimal places that you would like to use and voila! Load your wallet and move on to the crowd sale!</li>
                 </ul>
               </Tab.Pane>
-              <Tab.Pane eventKey="crowd">
+              <Tab.Pane eventKey="Crowdfunding *Use at your own risk*">
                 <ul>
-                  <li><strong>THIS IS WHERE THE REAL MAGIC HAPPENS!</strong> </li>
-                  <li> Set a price per token and a funding goal (make sure that you leave your self a ton of tokens so you can be rich)  </li>
-                  <li> You will unlock your account, enter the <u>Price per token</u>, and the <u>funding goal</u>.</li>
-                  <li> Start the ICO and pump, Pump, PUMP IT UP!!!!!</li>
-                  <li> Be sure to post how much money you are making everywher to bring the ICO machine more users</li>
+                  <li> You will unlock your account, enter the price per token, and your fundraising goal. Now those that wish to purchase your token can do so by sending ETC or ETH at your designated price to the contract address.</li>
+                  <li> US financial regulations are very strict on how funds can and can not be raised. You are not clever because you are being sneaky and using the blockchain, scammers have been doing the same thing for 100 years.</li>
+                  <li> Even though you may not live in the USA. Through international treaties, that doesnt matter if you are still soliciting to its citizens. Your conuntry most likely has similar laws you are violiating such as anti-spamming and reporting rules.</li>
                 </ul>              
               </Tab.Pane>
-              <Tab.Pane eventKey="more">
+              <Tab.Pane eventKey="References for the Legal Use of Initial Coin Offerings">
                 <ul>
-                   <li>What you are doing is most likely a crime. US financial regulations are very strict on how funds can and can not be raised. You are not clever because you are being sneaky and using the blockchain, scammers have been doing the same thing for 100 years.</li>
-                   <li> I get it, you don't <underline>live</underline> in the USA. Through international treaties, that doesnt matter as you are still soliciting to its citizens. Your conuntry most likely has similar laws you are violiating such as anti0spamming and reporting rules.</li>
                     <li><a href="https://www.sec.gov/news/pressrelease/2015-249.html" target="_blank">Crowdfunding in the USA: Jobs act Title III</a></li>
-                    <li><a href="http://consumer.findlaw.com/securities-law/what-is-the-howey-test.html" target="_blank">What is a security: the Howey Test</a></li>
+                    <li><a href="http://securities-law-blog.com/2014/11/25/what-is-a-security-the-howey-test-and-reves-test/" target="_blank">What is a security: the Howey Test</a></li>
                 </ul>
               </Tab.Pane>
             </Tab.Content>
