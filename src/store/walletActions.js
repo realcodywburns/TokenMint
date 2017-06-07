984ca6ac7e30350c91eea789b341068a8c1af961
Elaine Ou <elaine.ou@gmail.com>
2017-06-07 14:49:28 -0700

    store/walletActions: fetch txdata on opening wallet

--- src/store/walletActions.js
+++ src/store/walletActions.js
@@ -1,13 +1,13 @@
 import { Wallet } from '../lib/wallet';
+import { getTransactionData } from './transactionActions';
 
 export function openWallet(key) {
     return (dispatch) => {
-        console.log(key)
         const wallet = new Wallet(key);
-        console.log(wallet)
         dispatch({
             type: 'WALLET/OPEN',
             wallet: wallet,
         });
+        dispatch(getTransactionData(wallet.getAddressString()));
     };
 }
