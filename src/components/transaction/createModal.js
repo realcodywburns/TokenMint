d419bc8c1a53aaa1a9f1d24a885db9be5299e16e
Elaine Ou <elaine.ou@gmail.com>
2017-06-07 12:51:16 -0700

    components/transaction/createModal: repair modal

--- src/components/transaction/createModal.js
+++ src/components/transaction/createModal.js
@@ -1,48 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Modal, Button, Collapse } from 'react-bootstrap';
+import { Well, Modal, Button, Collapse } from 'react-bootstrap';
 
-class TxModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showRaw: false
-    };    
-  }
+export const CreateTxModal = (props) => {
+    const { show, close, onGenerate, showRaw } = props;
+    const { rawTx, signedTx } = props;
 
-  render() {
     return (
-      <Modal bsSize="large">
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-lg">Create Transaction</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>You are about to Create a token on the {this.props.chain} chain.</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.props.onGenerate}>Generate Transaction</Button>
-          <Button onClick={this.props.onHide}>No!</Button>
-        </Modal.Footer>
-
-        <Collapse in={this.state.showRaw}>
-          raw transaction here
-        </Collapse>
-      </Modal>
+      <Modal show={show} onHide={close} bsSize="large">
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-lg">Create Transaction</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>You are about to Create a token on the Ethereum Classic chain.</p>
+            <Button bsStyle="primary" onClick={onGenerate}>Generate Transaction</Button>
+          </Modal.Body>
+          {showRaw && <Modal.Footer>
+            <Well>{rawTx}</Well>  
+            <Well>{signedTx}</Well>  
+          </Modal.Footer>}
+        </Modal>
     );
-  }
 };
-
-const CreateTxModal = connect(
-  (state, ownProps) => {
-    return {
-      chain: "Ethereum Classic"
-    }
-  },
-  (dispatch, ownProps) => ({
-    onGenerate: () => {
-      console.log("Generate Transaction")
-    }
-  })
-)(TxModal)
-
-export default CreateTxModal;
