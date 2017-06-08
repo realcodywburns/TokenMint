a2595cfe7fcb8f16a0a8398219f8f16dc6e76650
Elaine Ou <elaine.ou@gmail.com>
2017-06-07 17:14:26 -0700

    components/tokens/create: estimate gas

--- src/components/tokens/create.js
+++ src/components/tokens/create.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
-import { generateTokenTransaction } from '../../store/tokenActions';
+import { Panel, Form, FormGroup, ControlLabel, FormControl, Button } from 'react-bootstrap';
+import { generateTokenTransaction, estimateTokenGas } from '../../store/tokenActions';
 import { CreateTxModal } from '../transaction/createModal';
+import OpenWallet from '../wallet/open';
+
 
 class CreateTokenForm extends React.Component {
   constructor(props) {
     super(props);
     this.initToken = this.initToken.bind(this);
+    this.estimateGas = this.estimateGas.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.state = {
       symbol: 'POOP',
       decimals: 8,
       modalShow: false, 
+      showTx: false,
+      tx: {},
     };
   }
 
@@ -25,6 +30,24 @@ class CreateTokenForm extends React.Component {
     this.setState({ [e.target.id]: e.target.value });
   }
 
+  estimateGas() {
+    const data = {
+      token: this.state.token,
+      symbol: this.state.symbol,
+      totalSupply: this.state.totalSupply,
+      decimals: this.state.decimals,
+    }
+    this.props.estimateGas(data, this.props.wallet)
+      .then((result) => { 
+        //const tx = JSON.parse(result);
+        console.log(result)
+        this.setState({ modalShow: true, 
+                        showTx: true,
+                        //tx
+                      });
+      })
+  }
+
   initToken() {
     const data = {
       token: this.state.token,
@@ -33,9 +56,13 @@ class CreateTokenForm extends React.Component {
       decimals: this.state.decimals,
     }
     this.props.initToken(data, this.props.wallet)
-      .then((result) => {
-        console.log(result);
-        this.setState({ modalShow: true});
+      .then((result) => { 
+        //const tx = JSON.parse(result);
+        console.log(result)
+        this.setState({ modalShow: true, 
+                        showTx: true,
+                        //tx
+                      });
       })
   }
 
@@ -43,71 +70,82 @@ class CreateTokenForm extends React.Component {
     let modalClose = () => this.setState({ modalShow: false });
 
     return (
-      <Form>
-        <FormGroup
-          controlId="token"
-          validationState={this.getRequiredValidation('token')}
-        >
-          <ControlLabel>Token Name</ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.token}
-            placeholder="Scamcoin"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
+      <div>
+        <Form>
+          <FormGroup
+            controlId="token"
+            validationState={this.getRequiredValidation('token')}
+          >
+            <ControlLabel>Token Name</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.token}
+              placeholder="Scamcoin"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
 
-        <FormGroup
-          controlId="totalSupply"
-          validationState={this.getRequiredValidation('totalSupply')}
-        >
-          <ControlLabel>Total Supply</ControlLabel>
-          <FormControl
-            type="number"
-            value={this.state.totalSupply}
-            placeholder="1000000"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
+          <FormGroup
+            controlId="totalSupply"
+            validationState={this.getRequiredValidation('totalSupply')}
+          >
+            <ControlLabel>Total Supply</ControlLabel>
+            <FormControl
+              type="number"
+              value={this.state.totalSupply}
+              placeholder="1000000"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
 
-        <FormGroup
-          controlId="symbol"
-        >
-          <ControlLabel>Token Symbol (optional)</ControlLabel>
-          <FormControl
-            type="text"
-            value={this.state.symbol}
-            placeholder="POOP"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
+          <FormGroup
+            controlId="symbol"
+          >
+            <ControlLabel>Token Symbol (optional)</ControlLabel>
+            <FormControl
+              type="text"
+              value={this.state.symbol}
+              placeholder="POOP"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
 
-        <FormGroup
-          controlId="decimals"
-        >
-          <ControlLabel>Decimal Places (optional)</ControlLabel>
-          <FormControl
-            type="number"
-            value={this.state.decimals}
-            placeholder="8"
-            onChange={this.handleChange}
-          />
-          <FormControl.Feedback />
-        </FormGroup>
-
-        <FormGroup>
+          <FormGroup
+            controlId="decimals"
+          >
+            <ControlLabel>Decimal Places (optional)</ControlLabel>
+            <FormControl
+              type="number"
+              value={this.state.decimals}
+              placeholder="8"
+              onChange={this.handleChange}
+            />
+            <FormControl.Feedback />
+          </FormGroup>
+        </Form>
+        {this.props.wallet &&
           <Button 
             bsStyle="primary"
-            onClick={this.initToken} >
+            onClick={this.estimateGas} >
             LET'S DO THIS
-          </Button>
-        </FormGroup>
+          </Button>}
+        {!this.props.wallet && 
+          <Panel header="Please unlock your account to continue">
+              <OpenWallet />
+          </Panel>
+        }
 
-        <CreateTxModal show={this.state.modalShow} close={modalClose} />
-      </Form>
+        <CreateTxModal 
+          show={this.state.modalShow} 
+          close={modalClose} 
+          showTx={this.state.showTx}
+          rawTx={this.state.tx.rawTx}
+          signedTx={this.state.tx.signedTx}
+          />
+      </div>
     );
   }
 };
@@ -120,18 +158,37 @@ const CreateToken = connect(
     }
   },
   (dispatch, ownProps) => ({
-    initToken: (data, wallet) => {
+    estimateGas: (data, wallet) => {
       const confirmTx = (tx) => {
-        console.log(tx);
+        console.log(tx)
+        return tx;
       }
       const resolver = (resolve, f) => (x) => {
-          f.apply(x);
-          resolve(x);
+          return f.apply(x);
+          //resolve(x);
+      };
+
+      return new Promise((resolve, reject) => {
+        dispatch(estimateTokenGas( data, wallet ))
+        .then((result) => resolve(result))
+      })      
+    },
+    initToken: (data, wallet) => {
+      const confirmTx = (tx) => {
+        console.log(tx)
+        return tx;
+      }
+      const resolver = (resolve, f) => (x) => {
+          return f.apply(x);
+          //resolve(x);
       };
       return new Promise((resolve, reject) => {
         dispatch(
           generateTokenTransaction( data, wallet )
-        ).then(resolver(confirmTx, resolve));
+        ).then(resolver(confirmTx, resolve))
+         .catch((error) => {
+            resolve({ _error: (error.message || JSON.stringify(error)) });
+         })
       })
     }
   })
--- src/components/transaction/createModal.js
+++ src/components/transaction/createModal.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Well, Modal, Button, Collapse } from 'react-bootstrap';
 
 export const CreateTxModal = (props) => {
-    const { show, close, onGenerate, showRaw } = props;
+    const { show, close, onGenerate, showTx } = props;
     const { rawTx, signedTx } = props;
 
     return (
@@ -15,7 +15,7 @@ export const CreateTxModal = (props) => {
             <p>You are about to Create a token on the Ethereum Classic chain.</p>
             <Button bsStyle="primary" onClick={onGenerate}>Generate Transaction</Button>
           </Modal.Body>
-          {showRaw && <Modal.Footer>
+          {showTx && <Modal.Footer>
             <Well>{rawTx}</Well>  
             <Well>{signedTx}</Well>  
           </Modal.Footer>}
