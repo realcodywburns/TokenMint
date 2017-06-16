f32d40ca2ebffe50d7ba80639cc6bf92141896e4
Elaine Ou <elaine.ou@gmail.com>
2017-06-16 14:56:47 -0700

    store/tokenActions, lib/contract: move ABI constants

--- src/store/tokenActions.js
+++ src/store/tokenActions.js
@@ -1,64 +1,7 @@
-import Immutable from 'immutable';
 import { rpc } from '../lib/rpc';
 import { generateTx } from '../lib/transaction';
 import { functionToData, dataToParams, paramsToToken } from '../lib/convert';
-
-
-const IcoMachineAddress = "0x26c243b8a4a460a9bb20f3afcf127fa7dd764cfa";
-
-const CreateTokenFunc = Immutable.fromJS({
-    name:'createToken',
-    inputs:[{ name:'initialSupply', type:'uint256' },
-            { name:'tokenName', type:'string' },
-            { name:'decimals', type:'uint8' },
-            { name:'symbol', type:'string' }]
-    });
-
-const CreateSaleFunc = Immutable.fromJS({
-    name:'createSale',
-    inputs:[{ name:'fundingGoal', type:'uint' },
-            { name:'etherCostOfEachToken', type:'uint' }],
-    outputs:[{ name:'_saleAddress', type:'address' }]
-    });
-
-const TokensFunc = Immutable.fromJS({
-    name:'tokens',
-    inputs:[{ name: '', type: 'address' }],
-    outputs:[{name:"tokenAddress","type":"address"},
-            {name:"saleAddress","type":"address"},
-            {name:"initialSupply","type":"uint256"},
-            {name:"tokenName","type":"string"},
-            {name:"decimals","type":"uint8"},
-            {name:"symbol","type":"string"}]
-});
-
-const CrowdSaleFuncs = [
-    Immutable.fromJS({
-        name: "amountRaised",
-        inputs:[],
-        outputs:[{ name: '', type: 'uint'}]
-    }),
-    Immutable.fromJS({
-        name: "beneficiary",
-        inputs:[],
-        outputs:[{ name: '', type: 'address'}]
-    }),
-    Immutable.fromJS({
-        name: "fundingGoal",
-        inputs:[],
-        outputs:[{ name: '', type: 'uint'}]
-    }),
-    Immutable.fromJS({
-        name: "tokenPrice",
-        inputs:[],
-        outputs:[{ name: '', type: 'uint'}]
-    }),
-    Immutable.fromJS({
-        name: "tokenReward",
-        inputs:[],
-        outputs:[{ name: '', type: 'address'}]
-    })
-]
+import { IcoMachineAddress, CreateTokenFunc, CreateSaleFunc, TokensFunc, CrowdSaleFuncs } from '../lib/contract';
 
 const initialTx = {
     to: IcoMachineAddress,
