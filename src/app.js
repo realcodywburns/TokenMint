c2eac513dc4b71afe76b07d91adc31338d7c9436
Elaine Ou <elaine.ou@gmail.com>
2017-06-16 13:13:45 -0700

    src/index, app: add react-router

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux'
+import { Route } from 'react-router';
 import { store } from './store/store.js';
 import Main from './components/main';
+import About from './components/info/about';
 
 class TokenApp extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <div>
+            <Route exact path="/" component={Main}/>
+            <Route path="/about" component={About}/>
+        </div>
       </Provider>
     );
   }
