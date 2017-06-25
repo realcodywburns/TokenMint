a348121b450e8a8801c3899fdad7bbd4e2fc03c4
Elaine Ou <elaine.ou@gmail.com>
2017-06-25 15:27:32 -0700

    components/wallet/show: make way for send

--- src/components/wallet/show.js
+++ src/components/wallet/show.js
@@ -1,79 +1,65 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Grid, Row, Col, Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Row, Col, Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { toFiat, toEther } from '../../lib/etherUnits';
 import { gotoTab } from '../../store/tabActions';
+import { wrap } from '../../lib/styles';
 
 
 class RenderWallet extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      privKey: '',
-    };
-  }
- 
-
-
   render() {
-    return (
-      <Grid>
-        <Row>            
-          <Col sm={8} md={6}>
-            <Panel>
-              
-              <Panel header="Account Address" bsStyle="success">
-                {this.props.wallet.getAddressString()}
-              </Panel>
-              <Panel bsStyle="info">
-                <h4>Balance</h4>
-                {this.props.balance || '?'} ETC
-              </Panel>
-              <Panel bsStyle="success">
-                <ListGroup>
-                  {this.props.token && 
-                    <ListGroupItem header={`${this.props.token.get("name")}(${this.props.token.get("symbol")})`}>
-                    Token Contract: 
-                    <a href={`http://gastracker.io/addr/${this.props.token.get("tokenAddress")}`} 
-                      rel="noopener noreferrer"
-                      target="_blank">
-                      {this.props.token.get("tokenAddress")}
-                    </a><hr />
-                    Crowdsale Status: 
-                    {this.props.token.get("saleAddress") && 
-                      <Button bsSize="small" bsStyle="info" href={`/ico/${this.props.token.get("saleAddress")}`}>
-                      Active
-                    </Button>}
-                    {!this.props.token.get("saleAddress") && 
-                      <Button bsSize="small" bsStyle="success" onClick={this.props.gotoIco}>Launch ICO
-                      </Button>}
-                  </ListGroupItem>}
-                  {this.props.ico && 
-                    <ListGroupItem header={`${this.props.token.get("name")} Crowdsale`}>
-                    Funding Goal: {this.props.ico.get("fundingGoal")}
-                    <hr />
-                    Amount Raised: {this.props.ico.get("amountRaised")}
-                  </ListGroupItem>}
-                </ListGroup>
-              </Panel>
-              <Panel>
-                <h4>Equivalent Values</h4>
-                <hr />  
-                {this.props.fiatValues.map((v) =>
-                  <Row key={v.currency}><Col smOffset={1}>{v.value} {v.currency.toUpperCase()}</Col>
-                  </Row>)}
-              </Panel>
-              <Panel bsStyle="warning">
-                <h4>Tokens</h4>
-                Coming soon...
-              </Panel>
-              {this.props.showClose && <Button onClick={this.props.closeWallet}>Close Wallet</Button>}
-            </Panel>
-          </Col>
-        </Row>
-      </Grid>
+    return (        
+        <Panel>
+          
+          <Panel header="Account Address" bsStyle="success">
+            <span style={wrap}>
+              {this.props.wallet.getAddressString()}
+            </span>
+          </Panel>
+          <Panel bsStyle="info">
+            <h4>Balance</h4>
+            {this.props.balance || '?'} ETC
+          </Panel>
+          <ListGroup>
+            {this.props.token && 
+              <ListGroupItem header={`${this.props.token.get("name")}(${this.props.token.get("symbol")})`}>
+              Token Contract: 
+              <a href={`http://gastracker.io/addr/${this.props.token.get("tokenAddress")}`} 
+                rel="noopener noreferrer"
+                target="_blank">
+                <span style={wrap}>{this.props.token.get("tokenAddress")}</span>
+              </a><hr />
+              Crowdsale Status: 
+              {this.props.token.get("saleAddress") && 
+                <Button bsSize="small" bsStyle="info" href={`/ico/${this.props.token.get("saleAddress")}`}>
+                Active
+              </Button>}
+              {!this.props.token.get("saleAddress") && 
+                <Button bsSize="small" bsStyle="success" onClick={this.props.gotoIco}>Launch ICO
+                </Button>}
+            </ListGroupItem>}
+            {this.props.ico && 
+              <ListGroupItem header={`${this.props.token.get("name")} Crowdsale`}>
+              Funding Goal: {this.props.ico.get("fundingGoal")}
+              <hr />
+              Amount Raised: {this.props.ico.get("amountRaised")}
+            </ListGroupItem>}
+          </ListGroup>
+          <Panel>
+            <h4>Equivalent Values</h4>
+            <hr />  
+            {this.props.fiatValues.map((v) =>
+              <Row key={v.currency}><Col smOffset={1}>{v.value} {v.currency.toUpperCase()}</Col>
+              </Row>)}
+          </Panel>
+          <Panel bsStyle="warning">
+            <h4>Tokens</h4>
+            Coming soon...
+          </Panel>
+          {this.props.showClose && <Button onClick={this.props.closeWallet}>Close Wallet</Button>}
+        </Panel>
     );
   }
 }
