1208f1ad4ca804226a3261f654ff53cf41611301
Cody Burns <cody.w.burns@gmail.com>
2017-06-26 22:05:48 -0500

    fix
    update contract and fix second reg call

--- src/lib/contract.js
+++ src/lib/contract.js
@@ -4,7 +4,7 @@ export const TokenMintAddress = "0x0023BEBee6FAE12E3b387497300FEd1316e6905e";
 
 export const IcoMachineAddress = "0x2f846034a256f51ae51249b61f4c92bcf4b0a3d8";
 
-export const RegContractAddress = '0x0c33c443f4e01c52e411ae3e165f99934ba43ac5';
+export const RegContractAddress = '0xf4d139fE4f73725dcc6eb364e4593a00F07DC0a9';
 
 export const CreateTokenFunc = Immutable.fromJS({
     name:'createToken',
@@ -129,8 +129,8 @@ export const regData =  Immutable.fromJS({
                 {name:"_tURL",type:"string"},
                 {name:"_tBlerb",type:"string"}]
     });
-export const regData =  Immutable.fromJS({
-        name: "getArray",
+export const regQuick =  Immutable.fromJS({
+        name: "getQuick",
         inputs:[{name:"_index", type:"uint"}],
         outputs:[{ name: 'tAddr', type: 'address'},
                 { name: 'tName', type: 'string'},
