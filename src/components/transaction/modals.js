453b306f57ed3addaf8f17a82613808098eaeeb1
Elaine Ou <elaine.ou@gmail.com>
2017-07-30 11:24:15 -0700

    component/tx/modals: add register modal

--- src/components/transaction/modals.js
+++ src/components/transaction/modals.js
@@ -133,6 +133,44 @@ export const SendTxModal = (props) => {
     );
 };
 
+export const RegisterTxModal = (props) => {
+    const { show, close, gas, changeGas, onGenerate, showTx } = props;
+    const { rawTx, signedTx, submitTx } = props;
+
+    return (
+      <Modal show={show} onHide={close} bsSize="large">
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-lg">Create Transaction</Modal.Title>
+          </Modal.Header>
+          {!showTx && <Modal.Body>
+            <p>You are about to register a token on the Ethereum Classic chain.</p>
+            <FormGroup
+              controlId="gasLimit"
+            >
+              <ControlLabel>Gas</ControlLabel>
+              <FormControl
+                type="number"
+                value={gas}
+                onChange={changeGas}
+              />
+            </FormGroup>
+            <Button bsStyle="primary" onClick={onGenerate}>Generate Transaction</Button>
+          </Modal.Body>}
+          {showTx && <Modal.Body>
+              <FormGroup>
+                <ControlLabel>Raw Transaction</ControlLabel>
+                <Well bsSize="sm" style={code}>{rawTx}</Well>  
+              </FormGroup>
+              <FormGroup>
+                <ControlLabel>Signed Transaction</ControlLabel>
+                <Well bsSize="sm" style={code}>{signedTx}</Well>
+              </FormGroup>
+            <Button bsStyle="primary" onClick={submitTx}>Submit</Button> 
+          </Modal.Body>}
+        </Modal>
+    );
+};
+
 export const SuccessModal = (props) => {
     const { show, close } = props;
     const { hash } = props;
